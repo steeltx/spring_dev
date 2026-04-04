8e969ff3ea8a4455a323b86c145c753e6accd49c
steeltx <realsteel.gnu@gmail.com>
2026-04-04 00:14:02 -0600

    Backend portfolio - Init

---
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/model/Experience.java
@@ -0,0 +1,20 @@
+package com.portfolio.portfolio_backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Experience {
+    private Long id;
+    private String jobTitle;
+    private String companyName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String description;
+    private Long personalInfoId;
+}
---
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/model/PersonalInfo.java
@@ -0,0 +1,22 @@
+package com.portfolio.portfolio_backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonalInfo {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String profileDescription;
+    private String profileImageUrl;
+    private Integer yearsOfExperience;
+    private String email;
+    private String phone;
+    private String linkedinUrl;
+    private String githubUrl;
+}
